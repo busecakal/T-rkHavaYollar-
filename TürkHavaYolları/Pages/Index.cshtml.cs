6af2a65fd96a9e8fa014cae0c47da1798221fe43
busecakal <48867424+busecakal@users.noreply.github.com>
2023-05-15 03:05:09 +0300

    Proje dosyası ekle.

---
+++ TürkHavaYolları/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public string Username { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
+
+    public IActionResult OnPostLogin()
+    {
+        if (Username == "kullaniciadi" && Password == "sifre123")
+        {
+            // Kullanıcı doğru kimlik bilgilerini girdiyse,
+            // Yolcu bilgileri sayfasına yönlendir.
+            return RedirectToPage("/YolcuBilgileri");
+        }
+        else
+        {
+            // Kullanıcı yanlış kimlik bilgilerini girdiyse,
+            // Hata mesajını görüntüle.
+            ModelState.AddModelError(string.Empty, "Kullanıcı adı veya şifre yanlış.");
+            return Page();
+        }
+    }
+}
